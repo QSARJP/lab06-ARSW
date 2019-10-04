df2ca99c3409a101b87267b2065fe0e355069aa0
Juan Ospina <juan.ospina-h@mail.escuelaing.edu.co>
2019-10-04 18:17:59 -0500

    prueba

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -2,20 +2,64 @@ const Url = 'http://localhost:8080/blueprints/';
 apiclient = (function () {
     var f=[]
     return {
-        getBlueprintsByAuthor: function (name, callback) {
-                $.get(Url+name,function(data){
-                    f=data;
-                });
-                return callback(f)
+        getBlueprintsByAuthor: function(name, callback) {
+          var promise = jQuery.ajax({
+            url: apiUrl + name,
+            success: function(result) {
+              callback(result);
+            },
+            async: true
+          });
+          promise.then(
+            function() {
+              console.info("OK ");
+            },
+            function() {
+              alert("No se encuentra el author: " + name);
+            }
+          );
         },
         getBlueprintsByNameAndAuthor: function(author, name, callback) {
-            jQuery.ajax({
-                url: Url+author+"/"+name,
-                success: function (result) {
-                    callback(result);
-                },
-                async: true
-            });
-        }
-    };
-})();
+          var promise = jQuery.ajax({
+            url: apiUrl + author + "/" + name,
+            success: function(result) {
+              callback(result);
+            },
+            async: true
+          });
+          promise.then(
+            function() {
+              console.info("OK ");
+            },
+            function() {
+              alert(
+                "No se encuentra el author: " + author + " con el plano: " + name
+              );
+            }
+          );
+        },
+        setBlueprint: function(author, name, blueprint) {
+          var promise = $.ajax({
+            url: "/blueprints/" + author + "/" + name + "/",
+            type: "PUT",
+            data: blueprint,
+            contentType: "application/json"
+          });
+    
+          promise.then(
+            function() {
+              Run.updateListPlans(author);
+              if (name) {
+                Run.openPlane(author, name);
+              }
+            },
+            function() {
+              console.info("ERROR");
+            }
+          );
+        },
+      };
+    })();
+
+
+      
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,23 +2,25 @@ var api = apiclient;
 var Run = (function() {
   var nameAuthor;
   var listBlue = [];
-  var canvas;
-  var context;
-  var bolean;
+  var boolean = false;
+  var nombre ;
 
   var cambiarNombre = function(author) {
+    boolean = false;
     nameAuthor = author;
   };
 
   var actualizar = function(author) {
     cambiarNombre(author);
     $("#authorname").text(author);
+    clearCanvas(getCanvas());
     api.getBlueprintsByAuthor(author, generatetable);
-    canvas = document.getElementById("myCanvas");
-    context = canvas.getContext("2d");
+    
   };
 
   var pintarBlue = function(author, name) {
+    boolean = true;
+    nombre = name;
     api.getBlueprintsByNameAndAuthor(author, name, generateCanvas);
   };
 
@@ -34,6 +36,58 @@ var Run = (function() {
     }, 0);
     $("#sumatotal").text(suma);
   };
+
+
+  var getCanvas = function() {
+    var c = document.getElementById("myCanvas");
+    return c;
+  };
+
+  var getOffset = function(obj) {
+    var offsetLeft = 0;
+    var offsetTop = 0;
+    do {
+      if (!isNaN(obj.offsetLeft)) {
+        offsetLeft += obj.offsetLeft;
+      }
+      if (!isNaN(obj.offsetTop)) {
+        offsetTop += obj.offsetTop;
+      }
+    } while ((obj = obj.offsetParent));
+    return { left: offsetLeft, top: offsetTop };
+  };
+
+  var reDibujar = function(event, canvas) {
+    var offset = getOffset(canvas);
+    var x = event.pageX - parseInt(offset.left, 10);
+    var y = event.pageY - parseInt(offset.top, 10);
+    var blueprint = listBlue.filter(obj => {
+      return obj.name === _name;
+    })[0];
+    blueprint.points.push({ x: x, y: y });
+    console.log(blueprint);
+    generateCanvas(blueprint);
+  };
+
+  var clearCanvas = function(canvas) {
+    var contex = canvas.getContext("2d");
+    contex.clearRect(0, 0, canvas.width, canvas.height);
+    contex.beginPath();
+    return contex;
+  };
+
+  var listenMouse = function() {
+    var canvas = _getCanvas();
+    var contx = canvas.getContext("2d");
+    if (window.PointerEvent) {
+      canvas.addEventListener("pointerdown", function(event) {
+        if (boolean) {
+          reDibujar(event, canvas);
+        }
+      });
+    }
+  };
+
 
   var generatetable = function(blueprints) {
     blueprints = mapPoints(blueprints);
@@ -53,8 +107,7 @@ var Run = (function() {
 
   var generateCanvas = function(blueprint) {
     $("#currentBlueprint").text(blueprint.name);
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
+    var context = clearCanvas(getCanvas());
     var anterior;
     blueprint.points.map(function(point) {
       if (!anterior) {
@@ -66,39 +119,34 @@ var Run = (function() {
       }
     });
   };
-  var init = function(){
-    
 
-      
-    console.info('initialized');
-    
-    //if PointerEvent is suppported by the browser:
-    if(window.PointerEvent) {
-      canvas.addEventListener("pointerdown", function(event){
-        alert('pointerdown at '+event.pageX+','+event.pageY);  
-        
-      });
-    }
-    else {
-      canvas.addEventListener("mousedown", function(event){
-                  alert('mousedown at '+event.clientX+','+event.clientY);  
+  var planoNuevo = function(name) {
+    $("#nameBlueprint").val("");
+    var blueprint = {
+      author: nameAuthor,
+      points: [],
+      name: name
+    };
+    api.setBlueprint(nameAuthor, nombre, JSON.stringify(blueprint));
+  };
 
-        }
-      );
-    }
-  };    
+  var savePoints = function() {
+    var blueprint = listBlue.filter(obj => {
+      return obj.name === nombre;
+    })[0];
+    api.setBlueprint(nameAuthor, nombre, JSON.stringify(blueprint));
+  };
 
-  var validacion = function(){
-
-  }
-
-
+  
 
   
 
   return {
     actualizar: actualizar,
     pintarBlue: pintarBlue,
-    init: init
+    listenMouse: listenMouse,
+    savePoints: savePoints,
+    planoNuevo: planoNuevo
   };
 })();
+
